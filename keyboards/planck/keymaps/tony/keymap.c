d5844762c38da4fe3e633c3a50f2aa8048e2fead
Anthony Cheng <tonyzxvc@gmail.com>
2017-04-22 14:26:29 -0400

    moved backspace to top-right

--- keyboards/planck/keymaps/tony/keymap.c
+++ keyboards/planck/keymaps/tony/keymap.c
@@ -56,10 +56,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {MY_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, _______, KC_LALT, LOWER,   MY_LSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL, KC_LGUI, _______, KC_LALT, LOWER,   KC_RSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LBRC,   KC_RBRC, _______},
+  {KC_TILD, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LBRC,   KC_RBRC, KC_DEL},
   {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,   KC_RPRN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR,   KC_RCBR, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END}
@@ -92,10 +92,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  _______,   _______, _______},
+  {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  _______,   _______, KC_DEL},
   {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,    _______},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   _______, _______},
-  {_______, _______, _______, _______, _______, MY_DEL,  KC_INS,  _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END}
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSLS, _______, _______,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, KC_INS,  _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END}
 },
 
 /* Adjust (Lower + Raise)
--- keyboards/planck/keymaps/tony/readme.md
+++ keyboards/planck/keymaps/tony/readme.md
@@ -1,2 +1,17 @@
 # The Default Planck Layout
 
+## Reference for make command options
+https://github.com/qmk/qmk_firmware/wiki/FAQ-Build
+
+make
+make dfu
+
+this also works:
+
+make planck-tony-dfu
+
+to restore to default:
+
+make planck-default-dfu
+
+(put the planck in programming mode with LOWER + RAISE + Q)
